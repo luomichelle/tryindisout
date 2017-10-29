20fb61735537ee7d4a96c59de4afb73b646ddeb4
Michelle <femichelleluo@gmail.com>
2017-10-29 10:13:16 -0700

    sa

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router  = express.Router();
 var helper = require('sendgrid').mail;
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 router.get('/', function(req, res) { 
 	res.redirect('/index');
@@ -12,18 +13,21 @@ router.get('/index', function(req,res) {
   });
 });
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('test@example.com');
-var to_email = new helper.Email('mluo0301@gmail.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'Hello, Email!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
-
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// var from_email = new helper.Email('test@example.com');
+// var to_email = new helper.Email('mluo0301@gmail.com');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var content = new helper.Content('text/plain', 'Hello, Email!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
 
 
 router.post('/send',function(req,res){
-  console.log("go")
+  var from_email = new helper.Email(req.body.email);
+  var to_email = new helper.Email('mluo0301@gmail.com');
+  var from_name = req.body.name;
+  var subject = req.body.subject;
+  var content = new helper.Content('text/plain', req.body.message);
+  var mail = new helper.Mail(from_email, subject, from_name, to_email, content);
+
 
   var request = sg.emptyRequest({
     method: 'POST',
